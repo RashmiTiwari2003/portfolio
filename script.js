93eaf8f813439afce7164a7f48a9e66408362c93
Rashmi <rashmitiwari35832@gmail.com>
2024-02-26 16:39:08 +0530

    Site Layout Complete

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-console.log("hello")
-console.log("HI")
+const arr = ['Student','Engineer','Developer']
+const quality = document.querySelector('#qualities')
+let i=0
+    setInterval(() => {
+        quality.innerText = arr[i]
+        i++
+        if (i>=3)
+        {
+        i=0
+        }
+}, 1000);
+
+
+// function timeout(inter) {
+//     return new Promise((resolve) => setTimeout(resolve, inter))
+// }
+
+// async function func()
+// {
+//     let i=0
+//     await timeout(0)
+//     while(i<3)
+//     {
+//         console.log(i)
+        
+//         await timeout(1000)
+//     }
+//     // func()
+// }
